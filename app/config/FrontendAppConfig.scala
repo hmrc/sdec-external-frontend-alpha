384087bc26329f0be0921ee1499b3e6a3d27d80b
ramindur.singh <282824933+ramindursingh@users.noreply.github.com>
2026-07-16 08:56:12 +0100

    SDEC-379: Copied files

--- app/config/FrontendAppConfig.scala
+++ app/config/FrontendAppConfig.scala
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2026 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package config
 
 import com.google.inject.{Inject, Singleton}
@@ -21,8 +37,10 @@ class FrontendAppConfig @Inject() (configuration: Configuration) {
   val loginContinueUrl: String = configuration.get[String]("urls.loginContinue")
   val signOutUrl: String       = configuration.get[String]("urls.signOut")
 
-  private val exitSurveyBaseUrl: String = configuration.get[Service]("microservice.services.feedback-frontend").baseUrl
-  val exitSurveyUrl: String             = s"$exitSurveyBaseUrl/feedback/sdec-external-frontend-alpha"
+  private val exitSurveyBaseUrl: String =
+    configuration.get[Service]("microservice.services.feedback-frontend").baseUrl
+  val exitSurveyUrl: String =
+    s"$exitSurveyBaseUrl/feedback/sdec-external-frontend-alpha"
 
   val languageTranslationEnabled: Boolean =
     configuration.get[Boolean]("features.welsh-translation")
@@ -36,4 +54,8 @@ class FrontendAppConfig @Inject() (configuration: Configuration) {
   val countdown: Int = configuration.get[Int]("timeout-dialog.countdown")
 
   val cacheTtl: Long = configuration.get[Int]("mongodb.timeToLiveInSeconds")
+
+  // SDEC URLS
+  val threadInformationApi: String =
+    s"${configuration.get[Service]("microservice.services.sdec-threadinfo-api-alpha").baseUrl}${configuration.get[String]("microservice.services.sdec-threadinfo-api-alpha.startUrl")}"
 }
---
+++ project/CodeCoverageSettings.scala
@@ -0,0 +1,42 @@
+import sbt.Setting
+import scoverage.ScoverageKeys
+
+object CodeCoverageSettings {
+  private val excludedPackages: Seq[String] = Seq(
+    "<empty>",
+    "Reverse.*",
+    ".*.Module",
+    "config.*",
+    "uk.gov.hmrc.BuildInfo",
+    "app.*",
+    "prod.*",
+    ".*Routes.*",
+    "testOnly.*",
+    "testOnlyDoNotUseInAppConf.*",
+    ".*handlers.*",
+    ".*components.*",
+    ".*viewmodels.govuk.*",
+    "controllers.LanguageSwitchController",
+    "models.UserAnswers",
+    "pages.*",
+    "queries.*",
+    ".*Algebra",
+    "repositories.*",
+    "viewmodels.*",
+    "views.html.*",
+    "views.ViewUtils",
+    "views.html.ErrorTemplate",
+    "models.Enumerable",
+    "models.Mode",
+    "forms.mappings.Formatters",
+    "views.html.IndexView",
+    "views.html.CheckYourAnswersView"
+  )
+
+  val settings: Seq[Setting[_]] = Seq(
+    ScoverageKeys.coverageExcludedPackages := excludedPackages.mkString(";"),
+    ScoverageKeys.coverageMinimumStmtTotal := 50,
+    ScoverageKeys.coverageFailOnMinimum    := true,
+    ScoverageKeys.coverageHighlighting     := true
+  )
+}

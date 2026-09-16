a617dd801358024a58d9d1f8209defc985be6f7a
Nikolas <gianick@users.noreply.github.com>
2026-09-16 09:30:59 +0100

    hmrc-mongo-play-30 update

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -3,7 +3,7 @@ import sbt.*
 object AppDependencies {
 
   private val bootstrapVersion = "10.7.0"
-  private val hmrcMongoVersion = "2.12.0"
+  private val hmrcMongoVersion = "2.14.0"
   private val hmrcPlayFrontend = "13.13.0"
   private val scalaChkVersion  = "3.2.17.0"
   private val awsSdkVersion    = "2.47.5"

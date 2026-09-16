59d6a47f4b4a720d88b7480eb5e19eb722e3745c
Nikolas <gianick@users.noreply.github.com>
2026-09-16 10:48:33 +0100

    bootstrap-frontend-play-30 update

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -2,7 +2,7 @@ import sbt.*
 
 object AppDependencies {
 
-  private val bootstrapVersion = "10.7.0"
+  private val bootstrapVersion = "10.8.0"
   private val hmrcMongoVersion = "2.14.0"
   private val hmrcPlayFrontend = "13.13.0"
   private val scalaChkVersion  = "3.2.17.0"
